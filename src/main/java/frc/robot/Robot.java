b108c98cde871115432391bde125b348dc4d812b
ElProfeAlviso <alejandro.alviso@tec.mx>
2024-09-05 09:47:16 -0600

    Comentarios de las diferentes funciones

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,12 +1,12 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// TITANIUM RAMS 5959, Chasis de practica de tipo ARCADE DRIVE 
+//Codigo para Robot de Reto Crescendo 2024
+// Code by: Danna, Denis y Prof. Alviso. 
 
-package frc.robot;
+package frc.robot; //Este es el nombre del paquete principal del codigo del robot.
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
-import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot; //Este es el framework para codificar un robot tipo Timed Robot.
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;//Esta Es el Driver de motores PWM Victors
+import edu.wpi.first.wpilibj.PS4Controller;//Esta es la libreria para usar un control de PS4
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,40 +18,41 @@ public class Robot extends TimedRobot {
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
+   * Los modos periodicos se ejecutan cada 20 ms.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {} //Se ejecuta una vez al encender el robot
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {}//Se ejecuta en loop mientras el robot esta encendido
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {}//Se ejecuta una vez al habilitar el modo autonomo
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {}//Se ejecuta en loop mientras el modo autonomo este habilitado
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {}//Se ejecuta una vez al habilitar el modo teleoperado
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {}//Se ejecuta en loop mientras el modo teleoperado este activado.
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {}//Se ejecuta una vez al deshabilitar el robot
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {}//Se ejecuta en loop al estar deshabilitado el robot.
 
   @Override
-  public void testInit() {}
+  public void testInit() {}//Se ejecuta una vez al habilitar el modo test.
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {}//Se ejecuta en loop al estar habilitado el modo test.
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {}//Se ejecuta una vez al iniciar el simulador
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {}//Se ejecuta en loop al habilitar el simulador.
 }

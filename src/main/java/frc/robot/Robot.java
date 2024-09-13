aebd53a4acec248b3ed07e164ab3a2a8026a8477
CokeJLPV <a01413163@tec.mx>
2024-09-13 16:47:12 -0600

    Agregamos control de chassis con encoders y control ps4.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,9 +5,13 @@
 package frc.robot; //Este es el nombre del paquete principal del codigo del robot.
 
 import edu.wpi.first.wpilibj.TimedRobot; //Este es el framework para codificar un robot tipo Timed Robot.
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;//Esta Es el Driver de motores PWM Victors
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.PS4Controller;//Esta es la libreria para usar un control de PS4
-
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase;
+import java.math.*;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -20,11 +24,32 @@ public class Robot extends TimedRobot {
    * initialization code.
    * Los modos periodicos se ejecutan cada 20 ms.
    */
+  private final VictorSP motorizquierda = new VictorSP(0);
+  private final VictorSP  motorderecha = new VictorSP(1);
+  private final PS4Controller controlrojo = new PS4Controller(0);
+  private final DifferentialDrive chassis = new DifferentialDrive(motorizquierda, motorderecha);
+  private final Encoder encoderIzquierdo = new Encoder(0, 1,false,CounterBase.EncodingType.k4X);
+  private final Encoder encoderDerecho = new Encoder(2, 3,true, CounterBase.EncodingType.k4X);
   @Override
-  public void robotInit() {} //Se ejecuta una vez al encender el robot
+  public void robotInit() {
+
+    motorderecha.setInverted(true);
+    encoderDerecho.setSamplesToAverage(5);
+    encoderIzquierdo.setSamplesToAverage(5);
+
+    encoderDerecho.setDistancePerPulse(1/360*Math.PI*6);
+    encoderIzquierdo.setDistancePerPulse(1/360*Math.PI*6);
+
+    encoderDerecho.setMinRate(1);
+    encoderIzquierdo.setMinRate(1);
+
+  } //Se ejecuta una vez al encender el robot
 
   @Override
-  public void robotPeriodic() {}//Se ejecuta en loop mientras el robot esta encendido
+  public void robotPeriodic() {
+    
+
+  }//Se ejecuta en loop mientras el robot esta encendido
 
   @Override
   public void autonomousInit() {}//Se ejecuta una vez al habilitar el modo autonomo
@@ -36,7 +61,14 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}//Se ejecuta una vez al habilitar el modo teleoperado
 
   @Override
-  public void teleopPeriodic() {}//Se ejecuta en loop mientras el modo teleoperado este activado.
+  public void teleopPeriodic() {
+
+chassis.arcadeDrive(-controlrojo.getLeftY(), -controlrojo.getRightX());
+  SmartDashboard.putNumber("Encoder Distance CM derecho", Math.round(2.54 * encoderDerecho.getDistance()*100)/100d);
+  SmartDashboard.putNumber("Encoder Distance CM izquierdo", Math.round(2.54 * encoderIzquierdo.getDistance()*100)/100d);
+    
+
+  }//Se ejecuta en loop mientras el modo teleoperado este activado.
 
   @Override
   public void disabledInit() {}//Se ejecuta una vez al deshabilitar el robot
